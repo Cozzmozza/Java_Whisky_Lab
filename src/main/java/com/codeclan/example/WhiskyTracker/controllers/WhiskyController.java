ab754073197555c3116f15464e05e326f4d667fa
Colin Morrison <colinjmmorris@gmail.com>
2021-06-02 16:07:34 +0100

    Can find whisky by region

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -20,15 +20,19 @@ public class WhiskyController {
     public ResponseEntity<List<Whisky>> getAllWhiskies(
             @RequestParam(name = "year", required = false) Integer year,
             @RequestParam(name = "age", required = false) Integer age,
-            @RequestParam(name = "distilleryId", required = false) Long distilleryId
+            @RequestParam(name = "distilleryId", required = false) Long distilleryId,
+            @RequestParam(name = "region", required = false) String region
     ){
         if (year != null){
             return new ResponseEntity<List<Whisky>>(whiskyRepository.findByYear(year), HttpStatus.OK);
         }
         if (distilleryId != null && age != null){
-//            return new ResponseEntity<List<Whisky>>(whiskyRepository.findByWhiskiesAgeAndDistilleryId(age, distilleryId), HttpStatus.OK);
             return new ResponseEntity<List<Whisky>>(whiskyRepository.findByAgeAndDistilleryId(age, distilleryId), HttpStatus.OK);
-
+        }
+        if (region != null){
+//            Changing first letter of string to uppercase, to match DataLoader entries
+            String fixedRegion = region.substring(0, 1).toUpperCase() + region.substring(1);
+            return new ResponseEntity<List<Whisky>>(whiskyRepository.findByDistilleryRegion(fixedRegion), HttpStatus.OK);
         }
         return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
     }
--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
@@ -2,7 +2,6 @@ package com.codeclan.example.WhiskyTracker.repositories;
 
 import com.codeclan.example.WhiskyTracker.models.Whisky;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +11,8 @@ public interface WhiskyRepository extends JpaRepository<Whisky, Long> {
 
     List<Whisky> findByYear(Integer year);
 
-//    List<Whisky> findByWhiskiesAgeAndDistilleryId(Integer age, Long distilleryId);
     List<Whisky> findByAgeAndDistilleryId(Integer age, Long distilleryId);
+
+    List<Whisky> findByDistilleryRegion(String region);
 
 }
